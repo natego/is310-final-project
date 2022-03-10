abdec82dabc81072e76e6228d8590cdc8dd12bd8
nate gomez <nate@nates-MBP.localdomain>
2022-03-10 11:12:25 -0600

    commited

--- IS310assignments/nategomezadvancedpython.py
+++ IS310assignments/nategomezadvancedpython.py
@@ -1,3 +1,5 @@
+
+"""
 tool = {
     'python' : {
         '2015' : 9, 
@@ -37,5 +39,22 @@ for key, value in tool.items() :
         print('total popularity count', value['2015'] + value['2016'] + value['2017']
         + value['2018'] + value['2019'])
     else: 
-        print('not twitter')
-        
+        print('not twitter')     
+""""
+
+jane = "The Project Gutenberg EBook of Pride and Prejudice, by Jane Austen This eBook is for the use of anyone anywhere at no cost and with almost no restrictions whatsoever.  You may copy it, give it away or re-use it under the terms of the Project Gutenberg License included with this eBook or online at www.gutenberg.org Title: Pride and Prejudice Author: Jane Austen Posting Date: August 26, 2008 Release Date: June, 1998 Last Updated: March 10, 2018 Language: English Character set encoding: UTF-8 ***START OF THIS PROJECT GUTENBERG EBOOK PRIDE AND PREJUDICE*** Produced by Anonymous Volunteers PRIDE AND PREJUDICE By Jane Austen Chapter 1 It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife."
+
+x = jane.split("***")
+jane_cleaned = x[2]
+print(jane_cleaned)
+
+
+#assignment 2 
+#print content of csv file 
+import csv
+with open('tools_dh_proceedings.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
+
+
